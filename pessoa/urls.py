d0dfc9de2f44a78db02bac608b3fd358274b63c4
marcassim <marcelo.assim@gmail.com>
2022-05-10 15:52:23 -0300

    Criação do FORM / URL / VIEW para CONTATO

--- pessoa/urls.py
+++ pessoa/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
-from .views import ListaPessoaView, PessoaCreateView,PessoaUpdateView,PessoaDeleteView
+from .views import ListaPessoaView, PessoaCreateView, PessoaUpdateView, PessoaDeleteView
+from . import views
 
 urlpatterns = [
     path("", ListaPessoaView.as_view(), name="pessoa.index"),
     path("novo/", PessoaCreateView.as_view(), name="pessoa.novo"),
-    path("editar/<int:pk>", PessoaUpdateView.as_view(), name="pessoa.editar"),
-    path("remover/<int:pk>", PessoaDeleteView.as_view(), name="pessoa.remover"),
+    path("<int:pk>/editar/", PessoaUpdateView.as_view(), name="pessoa.editar"),
+    path("<int:pk>/remover/", PessoaDeleteView.as_view(), name="pessoa.remover"),
+    path("<int:pk>/contatos/", views.contatos, name="pessoa.contatos"),
 ]
